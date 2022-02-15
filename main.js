7ab6671c093b06e5a1d2a908f4440f4aa84acbaf
Ti-KM <vanshin0319@icloud.com>
2022-02-16 02:20:40 +0900

    FirebaseSetup

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyA4-u-SYNxoQqiz15CFgVkmk2JJCy2kNho",
+  authDomain: "sayen-a8208.firebaseapp.com",
+  projectId: "sayen-a8208",
+});
+
+var db = firebase.firestore();
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -6,6 +14,25 @@ var app = new Vue({
   methods:{
     kyoka :function(){
       console.log("確認");
+    },
+    fire :function(){
+      db.collection("Player").doc("Say!").set({
+        timestamp:firebase.firestore.FieldValue.serverTimestamp()
+      })
+      .then(()=>{
+        console.log(db.collection("Player").doc("Say!"));
+      })
+      .catch((error)=>{
+        console.log("error");
+      });
     }
+  },
+  mounted :function(){
+    console.log("aaaa");
+    var docRef = db.collection("Player").doc("Say!");
+    docRef
+        .onSnapshot((doc) => {
+            console.log("Current data: ", doc.data());
+        });
   }
 })
